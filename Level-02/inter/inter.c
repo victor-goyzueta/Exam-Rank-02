9a30aa95f811eab0c56e67d180a05b88cb8aa455
Víctor J. Goyzueta <167643295+victor-goyzueta@users.noreply.github.com>
2024-12-17 17:44:35 +0100

    update

--- Level-02/inter/inter.c
+++ Level-02/inter/inter.c
@@ -6,7 +6,7 @@
 /*   By: vgoyzuet <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/12 14:57:06 by vgoyzuet          #+#    #+#             */
-/*   Updated: 2024/12/16 21:49:54 by vgoyzuet         ###   ########.fr       */
+/*   Updated: 2024/12/17 17:43:43 by vgoyzuet         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,38 +17,35 @@ int	main(int argc, char **argv)
 	int		i;
 	int		j;
 	int		tmp_i;
-	char	tmp_c;
 
 	if (argc == 3)
 	{
 		i = 0;
-		j = 0;
-		while (argv[1][i] && argv[2][j])
+		while (argv[1][i] != '\0')
 		{
-			while (argv[1][i] != argv[2][j])
+			j = 0;
+			while (argv[2][j] != '\0' && argv[1][i] != argv[2][j])
 			{
 				j++;
 			}
 			if (argv[1][i] == argv[2][j])
 			{
-				tmp_c = argv[1][i];
-				tmp_i = i + 1;
-				while (argv[1][tmp_i] && tmp_c != argv[1][tmp_i])
+				tmp_i = 0;
+				while (tmp_i < i && argv[1][tmp_i] != argv[1][i])
 				{
 					tmp_i++;
 				}
-				if (tmp_i <= i && argv[1][tmp_i] == argv[1][i])
+				if (tmp_i == i)
 				{
-					write(1, &tmp, 1);
-					i++;
+					write(1, &argv[1][i], 1);
 				}
-				else
-					i++;
 			}
-			else
-				i++;
+			i++;
 		}
 	}
 	write(1, "\n", 1);
 	return (0);
 }
+
+//$>./inter ddf6vewg64f gtwthgdwthdwfteewhrtag6h4ffdhsd | cat -e
+//df6ewg4$
