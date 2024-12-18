209b64cf738732d72e8cc4317254ef85814d9b28
victor-goyzueta <vgoyzuet@c2r14s4.42madrid.com>
2024-12-18 21:04:52 +0100

    update lvl 2

--- Level-02/wdmatch/wdmatch.c
+++ Level-02/wdmatch/wdmatch.c
@@ -6,7 +6,7 @@
 /*   By: vgoyzuet <vgoyzuet@student.42madrid.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/18 19:13:11 by vgoyzuet          #+#    #+#             */
-/*   Updated: 2024/12/18 19:27:19 by vgoyzuet         ###   ########.fr       */
+/*   Updated: 2024/12/18 20:46:47 by vgoyzuet         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,26 +24,50 @@ size_t	ft_strlen(char *str)
 	return (i);
 }
 
-
-int main(int argc, char **argv)
+int	main(int argc, char **argv)
 {
-	int	i;
-	int	j;
+	int		i;
+	int		j;
+	size_t	count;
+	size_t	len1;
+	size_t	len2;
 
-	i = 0;
 	if (argc == 3)
 	{
-		if (ft_strlen(argv[1]) > ft_strlen(argv[2]))
+		len1 = ft_strlen(argv[1]);
+		len2 = ft_strlen(argv[2]);
+		if (len1 < len2)
 		{
-			write(1, "\n", 1);
-			return (0);
-		}
-		while (argv[1][i])
-		{
+			i = 0;
 			j = 0;
-			while (argv[2][j])
-				j++;
-			i++;
+			count = 0;
+			while (argv[1][i])
+			{
+				while (argv[2][j])
+				{
+					if (argv[1][i] == argv[2][j])
+					{
+						count++;
+						j++;
+						i++;
+					}
+					else
+						j++;
+				}
+				i++;
+			}
+			i = 0;
+			j = 0;
+			if (argv[1][i] && count == len1)
+			{
+				while (argv[2][j])
+				{
+					if (argv[1][i] == argv[2][j])
+						write(1, &argv[1][i], 1);
+					j++;
+				}
+				i++;
+			}
 		}
 	}
 	write(1, "\n", 1);
